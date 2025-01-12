7d58a8b5233b1ae10fba328e8fc10fdef87c0d62
MoneyCheongJx <moneycheongjx2002@gmail.com>
2025-01-13 01:10:27 +0800

    [All controllers] - Fix CORS by allowing cross origin from localhost:3000

--- src/main/java/com/example/vet_clinic_management_backend/controller/ClientController.java
+++ src/main/java/com/example/vet_clinic_management_backend/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.vet_clinic_management_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Client Module", description = "")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/client")
 public class ClientController {
     private final ClientService clientService;
--- src/main/java/com/example/vet_clinic_management_backend/controller/LabDiagnosticController.java
+++ src/main/java/com/example/vet_clinic_management_backend/controller/LabDiagnosticController.java
@@ -1,6 +1,7 @@
 package com.example.vet_clinic_management_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Lab Dianostic Module", description = "")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/lab")
 public class LabDiagnosticController {
     private final LabDiagnosticService labDiagnosticService;
